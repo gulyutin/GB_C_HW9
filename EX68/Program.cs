e8f7b6644ca0ecffc4953d6f9e272a22abef4a4a
Gulyutin <gulyutin@gmail.com>
2022-08-24 12:19:32 +0700

    EX68 solved

---
+++ EX68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления 
+// функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+Console.Write("Введи M: ");
+int m = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи N: ");
+int n = Convert.ToInt16(Console.ReadLine());
+A(n, m);
+            
+static int A(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return A(n - 1, 1);
+    if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+    return A(n,m);
+}
+Console.WriteLine(A(n,m));
