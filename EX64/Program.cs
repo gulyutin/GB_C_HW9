256fb7eb578745541d6b83126ab0111340edb0a7
Gulyutin <gulyutin@gmail.com>
2022-08-24 11:51:42 +0700

    EX64 solved

--- EX64/Program.cs
+++ EX64/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значения M и N. 
+// Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N.
+
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (N>M)
+for (int i = M; i <= N; i++)
+    Console.Write($" {i} ");
+else
+    Console.Write($"M долнжно быть меньше N ");
